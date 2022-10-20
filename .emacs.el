b8ecefb5274ef737e71255d1bc303338d09b20f8
Oliver <73624670+Qubit-Hex@users.noreply.github.com>
2022-10-19 21:27:44 -0600

    Update .emacs.el
    updated settings. i think it ok now.

--- .emacs.el
+++ .emacs.el
@@ -1,4 +1,7 @@
 ;; CONFIGURATION: V.0.1
+;;
+
+
 
 ;; window transparency setting
 (set-frame-parameter (selected-frame) 'alpha '(95 . 95))
@@ -11,14 +14,11 @@
 (setq make-backup-files nil)
 (setq create-lockfiles nil)
 (global-display-line-numbers-mode)  ; display line numbers within current buffer
-(global-set-key (kbd "C-x C-b") 'buffer-menu)
-
 
 (menu-bar-mode 1) ; SHOW THE MENU BAR.
 (tool-bar-mode 0) ; Disable tool bar
 (scroll-bar-mode -1) ; remove the scroll bar since neo tree gets fucked up.....
 (setq inhibit-splash-screen t)  ; disable the default emacs splash screen
-
 
 ;; ENABLE PACKAGE REPOS FOR THE PACKAGE MANAGER
 (eval-and-compile
@@ -31,7 +31,6 @@
     (package-refresh-contents)
     (package-install 'use-package)))
 
-
 ;; ENABLE CODE EXECUTATION IN ORG MODE WITH THE FOLLOWING LANAGUAGES.
 (org-babel-do-load-languages
  'org-babel-load-languages '(
@@ -40,49 +39,72 @@
 			     (R . t)
 			     ))
 
-;; ENABLE IVY MODE  
-(ivy-mode)
-(setq ivy-use-virtual-buffers t)
-(setq enable-recursive-minibuffers t)
+(require 'ibuf-ext)
+(add-to-list 'ibuffer-never-show-predicates "^\\*")
 
-;; enable this if you want `swiper' to use it
-;; (setq search-default-mode #'char-fold-to-regexp)
+(setq ibuffer-saved-filter-groups
+(quote (("default"
+("dired" (mode . dired-mode))
+("org" (name . "^.*org$"))
+
+("web" (or (mode . web-mode) (mode . js2-mode)))
+("shell" (or (mode . eshell-mode) (mode . shell-mode)))
+("mu4e" (name . "\*mu4e\*"))
+("programming" (or
+(mode . python-mode)
+(mode . c++-mode)))
+("emacs" (or
+(name . "^\\*scratch\\*$")
+(name . "^\\*Messages\\*$")))
+))))
 
 
+(add-hook 'ibuffer-mode-hook
+(lambda ()
+(ibuffer-auto-mode 1)
+(ibuffer-switch-to-saved-filter-groups "default")))
+
+;; don't show these
+;(add-to-list 'ibuffer-never-show-predicates "zowie")
+;; Don't show filter groups if there are no buffers in that group
+(setq ibuffer-show-empty-filter-groups nil)
+
+;; Don't ask for confirmation to delete marked buffers
+(setq ibuffer-expert t)
 
 ;; ==========================================
 ;;           CUSTOM KEYBOARD SHORTCUTS
 ;; =========================================
 
-;; DEFAULT KEY WILL BE <CTRL>c + <CUSTOM COMAND>
+;; REMAP C-x C-b to iBuffer since it works better
+;; than the default buffer management
+;;
 
-; find a file with fzf 
-(global-set-key (kbd "C-c p") 'counsel-find-file)
-
+(global-set-key (kbd "C-x C-b") 'ibuffer)
+; FIND a file with fzf 
+(global-set-key (kbd "C-c p") 'fzf-find-file)
 ;; TERMINAL CONTROL FLOW 
 (global-set-key  (kbd "C-c /") 'multi-term)
-
 ; toggle tree macs 
-(global-set-key (kbd "C-c e") 'treemacs)
+(global-set-key (kbd "C-c e") 'neotree-toggle)
 ; toggle the buffers
-(global-set-key (kbd "C-c b") 'buffer-menu)
-
-(global-set-key (kbd "C-c t") 'centaur-tabs--create-new-tab)
-(global-set-key (kbd "C-c n") 'centaur-tabs-forward)
-(global-set-key (kbd "C-c N") 'centaur-tabs-backward)
-
+(global-set-key (kbd "C-c b") 'ibuffer)
+;; save the current file
+(global-set-key (kbd "C-c s") 'save-buffer)
+;; kill the current buffer
+(global-set-key (kbd "C-c q") 'kill-current-buffer)
 
 ;; =============================================
 ;;          END OF CUSTOM KEYBOARD SHORTCUTS
 ;; =============================================
 
 
-
-
 ;; ==========================================================================
 ;;          Package START
 ;; =========================================================================
 
+
+;; centaur tabs / might get a rid of this mixed feeling. 
 (use-package centaur-tabs
   :ensure t
   :demand
@@ -93,38 +115,34 @@
   
  )
 
+;; enable mulit sessions of terminals 
 (use-package multi-term
   :ensure t
   :config
   (setq mult-term-program "/bin/bash")
   )
 
-		 
+
+;; make sure emacs knows about the user defined path vars. 
 (use-package exec-path-from-shell
   :ensure t
   :config
   (exec-path-from-shell-initialize))
 
+
+;; enable popup for auto complete of commands
 (use-package which-key
   :ensure t
   :config
-  (which-key-mode))
+  (which-key-mode)
+  )
+
 
 
 (use-package expand-region
   :ensure t
   :bind (("C-=" . er/expand-region)
 	 ("C--" . er/contract-region)))
-
-;; json-mode
-(use-package json-mode
-  :ensure t)
-
-;; ALL THE ICONS
-(use-package all-the-icons
-  :ensure t
-  :if (display-graphic-p)
-  )
 
 ;; web-mode CONFIGURATION FOR WEB FILES.
 (use-package web-mode
@@ -204,14 +222,12 @@
  )
 
 ;; TREE MACS FILE EXPLORER. 
-(use-package treemacs
+(use-package neotree
   :ensure t
-  :config
-  (treemacs-resize-icons 14)
   )
 
 
-;; ENABLE FUZZY SEARCHING OF FILES AND COMMANDS 
+;; ENABLE FUZZY commands  
  (use-package counsel
     :ensure t
     :config
@@ -219,8 +235,27 @@
       :ensure t)
     (ivy-mode 1)
     (setq ivy-height 20)
-    (setq ivy-re-builders-alist '((t . ivy--regex-fuzzy))))
+    (setq ivy-re-builders-alist '((t . ivy--regex-fuzzy)))
+    (setq ivy-use-virtual-buffers t)
+    (setq enable-recursive-minibuffers t) 
+    )
 
+;; ENABLE THE FUZZY SEARCHING OF THE PACKAGES WITHIN THW SYSTEM /
+(use-package fzf
+  :ensure t
+  :bind
+    ;; Don't forget to set keybinds!
+  :config
+  (setq fzf/args "-x --color bw --print-query --margin=1,0 --no-hscroll"
+        fzf/executable "fzf"
+        fzf/git-grep-args "-i --line-number %s"
+        ;; command used for `fzf-grep-*` functions
+        ;; example usage for ripgrep:
+        ;; fzf/grep-command "rg --no-heading -nH"
+        fzf/grep-command "grep -nrH"
+        ;; If nil, the fzf buffer will appear at the top of the window
+        fzf/position-bottom t
+        fzf/window-height 15))
 
 ;; TYPESCRIPT PACKAGE
 (use-package tide
@@ -231,9 +266,11 @@
          (before-save . tide-format-before-save)))
 
 
+
 ;; =============================================================================
 ;;        END OF PACKAGES 
 ;; ============================================================================
+
 
 ;; ENABLE REG EX SEARCHING
 (defun enable-minor-mode (my-pair)
