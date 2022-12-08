1fb102c5c1dac44465272690491af2fd9aa7f66e
Oliver <73624670+Qubit-Hex@users.noreply.github.com>
2022-12-08 10:32:21 -0600

    Update .emacs.el

--- .emacs.el
+++ .emacs.el
@@ -1,26 +1,27 @@
 ;; CONFIGURATION: V.0.1
 ;; AUTHOR+: QUBIT-HEX
-;; PURPOSE: TO MAKE A C/C++ AND WEB DEVELOPMENT IDE FOR MY NEED THAT I DON'T HATE.
 
 
-;; window transparency setting
-(set-frame-parameter (selected-frame) 'alpha '(95 . 95))
-(add-to-list 'default-frame-alist '(alpha . (95 . 95)))
+;; DISPLAY SETTINGS
+(set-frame-parameter (selected-frame) 'alpha '(92 . 92)) ; trasnparent setting
+(add-to-list 'default-frame-alist '(alpha . (92 . 92))) ; transparent settings
+(set-face-attribute 'default nil :height 88)
 (set-default 'truncate-lines t)
-(set-face-attribute 'default nil :height 88) ; adjust the front size of the application 
-(setq gc-cons-threshold 100000000)
-(setq read-process-output-max (* 1024 1024)) ;; 1mb
+(global-display-line-numbers-mode)  ; display line numbers within current buffer
+(menu-bar-mode 1) ; disable the menu 
+(tool-bar-mode 0) ; Disable tool bar
+(scroll-bar-mode -1) ; remove the scroll bar since neo tree gets fucked up....
+
+
+;; FILE BEHAVIOUR 
 (setq auto-save-default nil)
 (setq make-backup-files nil)
 (setq create-lockfiles nil)
-(global-display-line-numbers-mode)  ; display line numbers within current buffer
-(menu-bar-mode 1) ; SHOW THE MENU BAR.
-(tool-bar-mode 0) ; Disable tool bar
-(scroll-bar-mode -1) ; remove the scroll bar since neo tree gets fucked up.....
 (setq inhibit-splash-screen t)  ; disable the default emacs splash screen
 (setq-default tab-width 4  indent-tabs-mode nil) ; default tabbing  behaviour.
 
 
+;; PACKAGE LOCATIONS 
 (eval-and-compile
   (customize-set-variable
    'package-archives '(("org" . "https://orgmode.org/elpa/")
@@ -31,46 +32,25 @@
     (package-refresh-contents)
     (package-install 'use-package)))
 
-;; ENABLE CODE EXECUTATION IN ORG MODE WITH THE FOLLOWING LANAGUAGES.
+
+
+;; ORG BABEL CONFIGURATION FOR ORG FILES 
 (org-babel-do-load-languages
  'org-babel-load-languages '(
-			     (python . t)
-			     (sql . t)
-			     (R . t)
-			     ))
-
-;; I buffer configuration  
-(require 'ibuf-ext)
-(add-to-list 'ibuffer-never-show-predicates "^\\*")
-(setq ibuffer-saved-filter-groups
-      (quote (("default"
-               ("dired" (mode . dired-mode))
-               ("emacs_config" (name . "^.emacs$"))
-               ("org" (name . "^.*org$"))
-               ("web" (or (mode . web-mode) (mode . js2-mode)))
-               ("shell" (or (mode . eshell-mode) (mode . shell-mode)))
-               ("mu4e" (name . "\*mu4e\*"))
-               ("programming" (or (mode . python-mode) (mode . c++-mode)))
-               ("emacs" (or (name . "^\\*scratch\\*$") (name . "^\\*Messages\\*$"))))
-              )
-             )
-      )
-
-
-
-(add-hook 'ibuffer-mode-hook
-(lambda ()
-(ibuffer-auto-mode 1)
-(ibuffer-switch-to-saved-filter-groups "default"))
-)
-
-(setq ibuffer-show-empty-filter-groups nil)
-(setq ibuffer-expert t)
+			                 (python . t)
+                             (R . t)
+                             )
+ )
 
 ;; ============================================
 ;;          START CUSTOM COMMANDS
 ;; ===========================================
 
+
+(defun emacs-config-file()
+  "Open the init file"
+  (interactive)
+  (find-file user-init-file))
 
 (defun spawn-shell()
   "Open a new instance of eshell."
@@ -78,12 +58,11 @@
   (eshell 'N)
   )
 
-(defun open-config()
-  "Open the init file"
+(defun count-lines-in-repo()
+  "In order to count the lines of code within the git repo"
   (interactive)
-  (find-file user-init-file)
+  (shell-command "git ls-files | xargs cloc")
   )
-
 
 ;; ENABLE REG EX SEARCHING
 (defun enable-minor-mode (my-pair)
@@ -105,16 +84,17 @@
 ;; =========================================
 
 (global-set-key (kbd "C-c p") 'helm-find) ;; fuzzy finding
-(global-set-key (kbd "C-c f") 'helm-find-files) ; fuzzy browsing 
-(global-set-key  (kbd "C-c /") 'spawn-shell) ;; spawn-shell
-(global-set-key (kbd "C-c e") 'neotree-toggle) ;; neo tree toggle 
-(global-set-key (kbd "C-c b") 'ibuffer) ;; current buffers that are in use.
+(global-set-key (kbd "C-c e") 'neotree-toggle) ;; toggle neo tree
+(global-set-key (kbd "C-c f") 'helm-find-files) ; fuzzy browsing  
 (global-set-key (kbd "C-c s") 'save-buffer) ;; save the current buffer
 (global-set-key (kbd "C-c q") 'quit-window) ;; kill the current window and
+(global-set-key (kbd "C-c a") 'org-agenda) ;; toggle the org agenda menu  for easier access
+
 
 ;; =============================================
 ;;          END OF CUSTOM KEYBOARD SHORTCUTS
 ;; =============================================
+
 
 
 ;; ==========================================================================
@@ -135,7 +115,6 @@
   (which-key-mode)
   )
 
-
 (use-package expand-region
   :ensure t
   :bind (("C-=" . er/expand-region)
@@ -152,7 +131,9 @@
 	 ("\\.html\\'" . web-mode)
 	 ("\\.css\\'" . web-mode)
 	 ("\\.scss\\'" . web-mode)
-         ("\\.php\\'" . web-mode))
+     ("\\.php\\'" . web-mode)
+     ("\\.blade\\'" . web-mode) 
+     )
   :commands web-mode)
 
 
@@ -194,13 +175,20 @@
 	 )
   :commands lsp-deferred)
 
-
 ;; python server
 (use-package lsp-pyright
   :ensure t
   :hook (python-mode . (lambda ()
-                          (require 'lsp-pyright)
+                         (require 'lsp-pyright)
                           (lsp))))
+;; Java LSP
+(use-package lsp-java
+  :ensure t
+  :config (add-hook 'java-mode-hook 'lsp)
+  :hook (java-mode . (lambda ()
+                       (require 'lsp-java)
+                       (lsp)))
+  )
 
 ;; BULLET MODE FOR ORG MODE.
 (use-package org-bullets
@@ -216,6 +204,19 @@
   (projectile-mode +1)
   )
 
+;; all the icons
+(use-package all-the-icons
+  :ensure t
+  )
+
+;; neo tree
+(use-package neotree
+  :ensure t
+  :config
+  (setq neo-theme (if (display-graphic-p) 'icons 'arrow))
+  (setq neo-window-width 45)
+  )
+
 ;; helm
 (use-package helm
   :ensure t
@@ -229,25 +230,21 @@
   :preface (require 'helm-config)
   :config (helm-mode 1)
   )
-
-;; neo tree
-(use-package neotree
-  :ensure t
-  )
-
-;; I also like this theme package, too so lets include this too... 
+;; koalin themes 
 (use-package kaolin-themes
   :ensure t
-  :config
-  (load-theme 'kaolin-dark t)
   )
 
-;; all the themes from doom
+
+;; Doom themss
+
 (use-package doom-themes
   :ensure t
   )
 
 
-;; =============================================================================
-;;        END OF PACKAGES 
-;; ============================================================================
+
+;;REST CLIENT MODE
+(use-package restclient
+  :ensure t
+  )
